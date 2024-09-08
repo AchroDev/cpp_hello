02932049314a9206f1d111c48f6de4eefa0cfecf
AchroDev <achrodev@proton.me>
2024-09-07 22:03:57 -0500

    Added comment about change

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main(){
+
+    //Using the '\n' character is more performant than using std::endl
+
     std::cout << "Hello World" << '\n';
     std::cout << "AchroDev was here ~";
     return 0;
